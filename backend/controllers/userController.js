e2754cee71b6bf3868c0230ba6c33a043337d03f
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2023-03-02 13:26:32 +0000

    db connected

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,20 +1,21 @@
+import asyncHandler from "express-async-handler";
+
 // @desc Register a new user
 // @route /api/users
 // @access Public
-export const registerUser = (req, res) => {
+export const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   //   Validation
   if (!name || !email || !password) {
-    return res.status(400);
-    throw new Error("Ple");
+    return res.status(400).json({ message: "Please include all fields!" });
   }
   res.send("Register User");
-};
+});
 
 // @desc Login a new user
 // @route /api/users/login
 // @access Public
-export const loginUser = (req, res) => {
+export const loginUser = asyncHandler(async (req, res) => {
   res.send("Login User");
-};
+});
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import colors from "colors";
 import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
+import errorHandler from "./middleware/errorMiddleware.js";
 dotenv.config();
+
+// Connect to DataBase
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,6 +20,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoutes);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
