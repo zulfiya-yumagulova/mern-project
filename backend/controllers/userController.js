f72dcc3e98e1a26278976084b8706fb017bd482d
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2023-03-09 22:18:24 +0000

    authentication is working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -73,3 +73,16 @@ const generateToken = (id) => {
     expiresIn: "30d",
   });
 };
+
+// @desc get Current User
+// @route /api/users/me
+// @access Private
+
+export const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
+});
