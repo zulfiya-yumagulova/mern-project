dab7298da345e133df8ef7d5ad2e44b700291676
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2023-03-09 21:37:00 +0000

    initial commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,5 +48,18 @@ export const registerUser = asyncHandler(async (req, res) => {
 // @route /api/users/login
 // @access Public
 export const loginUser = asyncHandler(async (req, res) => {
-  res.send("Login User");
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Credentials");
+  }
 });
